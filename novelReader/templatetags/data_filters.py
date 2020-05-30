5a11fd661d10478ff05c042d90b7e8f46bcd8391
Xiangyu Chen <sxch3n@hotmail.com>
2020-05-29 20:36:31 -0700

    add filters

--- novelReader/templatetags/data_filters.py
+++ novelReader/templatetags/data_filters.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 from django import template
 
@@ -11,5 +11,20 @@ def get_val_by_key(dictionary: Dict, key):
 
 
 @register.filter(name='last_elem')
-def get_last_element(lst):
-    return lst[-1]
+def get_last_element(list):
+    return list[-1]
+
+@register.filter(name='last_idx')
+def get_last_idx(chapter_list: List):
+    # returns index in the database, not the index in the list.
+    # the chapter index in the database starts from 1,
+    # since the 0-index is for the novel content page
+    return len(chapter_list)
+
+@register.filter(name='minus_one')
+def minus_one(num:int):
+    return num-1
+
+@register.filter(name='add_one')
+def add_one(num:int):
+    return num+1
